cadded87b1fbe66b075464c008bdeddacc8c73d4
PavelMalta <shamashov87@mail.ru>
2021-04-04 18:00:10 +0300

    add disabled button for Counter component

--- src/component/Button/Button.tsx
+++ src/component/Button/Button.tsx
@@ -4,13 +4,14 @@ import s from "./Button.module.css"
 type ButtonPropsType = {
     title: string,
     onClick?: () => void
+    disabled?: boolean
 }
 
 export const Button = (props: ButtonPropsType) => {
 
     return (
      <div className={s.item}>
-         <button onClick={props.onClick}>{props.title}</button>
+         <button onClick={props.onClick} disabled={props.disabled}>{props.title}</button>
      </div>
     )
 }
--- src/component/Counter/Counter.tsx
+++ src/component/Counter/Counter.tsx
@@ -9,10 +9,15 @@ export const Counter = () => {
 
     const error = useSelector((state: AppRootStateType) => state.count.error)
     const value = useSelector((state:AppRootStateType) => state.count.value)
+    const maxValue = useSelector((state:AppRootStateType) => state.count.maxValue)
+    const minValue = useSelector((state:AppRootStateType) => state.count.minValue)
     const dispatch = useDispatch()
 
     const setIncrement = () => dispatch(setIncrementAC())
     const resetValue = () => dispatch(resetValueAC())
+
+    const disabledInc = () => value === maxValue
+    const disabledReset = () => value === minValue
 
     return (
         <div className={s.setting}>
@@ -22,8 +27,8 @@ export const Counter = () => {
                     : <div className={s.count}>{value}</div>}
             </div>
             <div className={s.button_container}>
-                <Button title={"inc"} onClick={setIncrement}/>
-                <Button title={"reset"} onClick={resetValue}/>
+                <Button title={"inc"} onClick={setIncrement} disabled={disabledInc()}/>
+                <Button title={"reset"} onClick={resetValue} disabled={disabledReset()}/>
             </div>
         </div>
     )
